0dd86fec3fcbef9da2ea4f1c364c1dbca77aaee5
kollin-brandenburg <kollin.brandenburg@oracle.com>
2017-12-20 11:21:09 -0600

    added dates

--- src/js/viewModels/incidents.js
+++ src/js/viewModels/incidents.js
@@ -10,23 +10,33 @@ define([
   'knockout',
   'jquery',
   'ojs/ojknockout',
-  'ojs/ojlabel',
-  'ojs/ojinputtext',
   'ojs/ojdatetimepicker',
   'ojs/ojselectcombobox',
-  'ojs/ojtimezonedata',],
- function(oj, ko, $) {
-   // $(function(){
-   //   ko.applyBindings(null, document.getElementById('form-container'));
-   // });
+  'ojs/ojtimezonedata',
+  'ojs/ojlabel'
+], function(oj, ko, $) {
 
-    function IncidentsViewModel() {
-      var self = this;
+  $(function() {
+    ko.applyBindings(new IncidentsViewModel(), document.getElementById('div1'));
+  });
+  function IncidentsViewModel() {
+    var self = this;
+    self.name = ko.observable("Kollin")
+    //Getting the current date
 
-      // Below are a subset of the ViewModel methods invoked by the ojModule binding
-      // Please reference the ojModule jsDoc for additional available methods.
 
-      /**
+    this.value = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date(2017, 1, 1)));
+    this.plannedStart = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date(2017, 1, 1)));
+    this.plannedImplementation = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date(2017, 1, 1)));
+    this.submissionDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date(2017, 1, 1)));
+
+
+
+
+    // Below are a subset of the ViewModel methods invoked by the ojModule binding
+    // Please reference the ojModule jsDoc for additional available methods.
+
+    /**
        * Optional ViewModel method invoked when this ViewModel is about to be
        * used for the View transition.  The application can put data fetch logic
        * here that can return a Promise which will delay the handleAttached function
@@ -37,11 +47,11 @@ define([
        * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
        * the promise is resolved
        */
-      self.handleActivated = function(info) {
-        // Implement if needed
-      };
+    self.handleActivated = function(info) {
+      // Implement if needed
+    };
 
-      /**
+    /**
        * Optional ViewModel method invoked after the View is inserted into the
        * document DOM.  The application can put logic that requires the DOM being
        * attached here.
@@ -50,12 +60,11 @@ define([
        * @param {Function} info.valueAccessor - The binding's value accessor.
        * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
        */
-      self.handleAttached = function(info) {
-        // Implement if needed
-      };
+    self.handleAttached = function(info) {
+      // Implement if needed
+    };
 
-
-      /**
+    /**
        * Optional ViewModel method invoked after the bindings are applied on this View.
        * If the current View is retrieved from cache, the bindings will not be re-applied
        * and this callback will not be invoked.
@@ -63,11 +72,11 @@ define([
        * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
        * @param {Function} info.valueAccessor - The binding's value accessor.
        */
-      self.handleBindingsApplied = function(info) {
-        // Implement if needed
-      };
+    self.handleBindingsApplied = function(info) {
+      // Implement if needed
+    };
 
-      /*
+    /*
        * Optional ViewModel method invoked after the View is removed from the
        * document DOM.
        * @param {Object} info - An object with the following key-value pairs:
@@ -75,17 +84,15 @@ define([
        * @param {Function} info.valueAccessor - The binding's value accessor.
        * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
        */
-      self.handleDetached = function(info) {
-        // Implement if needed
-      };
-    }
-    
+    self.handleDetached = function(info) {
+      // Implement if needed
+    };
+  }
 
-    /*
+  /*
      * Returns a constructor for the ViewModel so that the ViewModel is constructed
      * each time the view is displayed.  Return an instance of the ViewModel if
      * only one instance of the ViewModel is needed.
      */
-    return new IncidentsViewModel();
-  }
-);
+  return new IncidentsViewModel();
+});
